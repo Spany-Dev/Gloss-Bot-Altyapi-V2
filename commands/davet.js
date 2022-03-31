ccb4b0904cf96c3e72faf481a3223c685cf7fa30
Spany <99221704+Spany-Dev@users.noreply.github.com>
2022-03-31 21:16:42 +0300

    Update davet.js

--- commands/davet.js
+++ commands/davet.js
@@ -2,15 +2,11 @@
  
 const {MessageEmbed} = require('discord.js')
 
-// kodumun oruspu çocukları xD
-// Sizin yaptığınız botlar bizi tutamaz
-// ali istatsitik kodunu aldın demi ibne görmedimki hiç istatistiklerini yaparım yok lan benim yaptığım projeye geldiğinde
 module.exports = {
    name: 'davet',
    run: async(client, message, args) => {
-//no almadım hiç (ibne yalanı derler buna)
-     //vallaha aq
-const motion = new MessageEmbed()
+
+const spany = new MessageEmbed()
 
 .setColor("#3f007f")
 .setThumbnail("https://cdn.discordapp.com/attachments/778238163826900993/778654507944378398/31504ef441a60bf30ac38ecb522f7afb.webp")
@@ -18,7 +14,7 @@ const motion = new MessageEmbed()
 
 [Botu Davet Et](https://discord.gg/pinkcode)`)
 .setFooter(`Gloss Davet sistemi.`)
-return message.channel.send(motion)
+return message.channel.send(spany)
      
    }
 }
