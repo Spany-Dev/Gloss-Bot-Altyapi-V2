7de0123b400d5aa4322a60b316f23ae154a9c946
Spany <99221704+Spany-Dev@users.noreply.github.com>
2022-03-31 21:15:59 +0300

    Update bug-bildir.js

--- commands/bug-bildir.js
+++ commands/bug-bildir.js
@@ -1,9 +1,6 @@
  
 const {MessageEmbed} = require('discord.js')
 
-// kodumun oruspu çocukları xD
-// Sizin yaptığınız botlar bizi tutamaz
-
 module.exports = {
    name: 'bug-bildir',
    run: async(client, message, args) => {
@@ -13,7 +10,7 @@ module.exports = {
     message.channel.send("**:yess22: | Bugunuz Gönderildi. Bot Üzerindeki Bug Doğruysa Dikkate Alınacaktır.**")
      
      client.channels.cache.get("782589032495710208").send(`${message.author} (**${message.author.id} | ${message.author.tag}**)\nBug Mesajı: **${args.slice(0).join(' ')}**`)
-     // üsteki mesajı ayarlayın biz göremediğimizden salladık
+     
      
      
    }
