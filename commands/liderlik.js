6e0475b6f143be72894fc3e7c0e1f150b7877118
Spany <99221704+Spany-Dev@users.noreply.github.com>
2022-03-31 21:22:06 +0300

    Update liderlik.js

--- commands/liderlik.js
+++ commands/liderlik.js
@@ -3,8 +3,6 @@ const {MessageEmbed} = require('discord.js')
 const db = require('quick.db')
 const moment = require('moment')
 require('moment-duration-format')
-// kodumun oruspu çocukları xD
-// Sizin yaptığınız botlar bizi tutamaz
 
 module.exports = {
    name: 'liderlik',
@@ -20,19 +18,19 @@ module.exports = {
       }
    }).sort((x, y) => y.level - x.level); // bilen anlıyor zaten çakma kodcular ise bu ne aq
      
-  let anasikim = [] 
+  let bilmem = [] 
    for(var i in levels){
      let user = await client.users.fetch(levels[i].id);
-     anasikim.push({id: levels[i].id,  name: user.username, level: levels[i].level, xp: levels[i].xp})
+     bilmem.push({id: levels[i].id,  name: user.username, level: levels[i].level, xp: levels[i].xp})
    }
      const embed = new MessageEmbed()
      .setColor("#3f007f")
      .setTitle("Seviye Sıralama")
      var i = 0;
-  await anasikim.forEach(async s => { 
+  await bilmem.forEach(async s => { 
      await embed.addField(`${i + 1}. ${s.name}`, `**Level** - ${s.level || 0}\n**XP** - ${s.xp} / 100}`)  
     })
-     let index = anasikim.map(s => s.id).indexOf(message.author.id)
+     let index = bilmem.map(s => s.id).indexOf(message.author.id)
      embed.setFooter("Senin Sıran: "+ Number(index + 1))
     message.channel.send(embed)
   }
