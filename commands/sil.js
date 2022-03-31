74b00d8f87caa87a07b87f70b50a90221aee17fd
Spany <99221704+Spany-Dev@users.noreply.github.com>
2022-03-31 21:32:15 +0300

    Update sil.js

--- commands/sil.js
+++ commands/sil.js
@@ -2,40 +2,38 @@
  
 const {MessageEmbed} = require('discord.js')
 const db = require('quick.db')
-// kodumun oruspu çocukları xD
-// Sizin yaptığınız botlar bizi tutamaz
-// ali istatsitik kodunu aldın demi ibne görmedimki hiç istatistiklerini yaparım yok lan benim yaptığım projeye geldiğinde
+
 module.exports = {
-   name: 'sil',// bu arada üsteki mesaj clone olmuş siktir edin qwe.qw silv ghvjbjhnkjmeklşn
+   name: 'sil',
    run: async(client, message, args) => {//Ç:D
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return;
     let mesaj = args[0] 
-   const motion = new MessageEmbed()
+   const spany = new MessageEmbed()
     .setColor("#3f007f")
     .setAuthor("Gloss Bot", "https://images-ext-2.discordapp.net/external/upcVsk1w8rbo_jfrfun5j4Fi0YCd25WPM9pu4SG-NQs/https/cdn.discordapp.com/avatars/726040861602742324/ff5a9e9abdd36d5f2effcfd9876527e2.webp")
     .setDescription(`**:no2: | Lütfen Rakam Belirtiniz**`)
     .setFooter(`🔮 Tüm Hakları Saklıdır.`)
-    if(!mesaj || isNaN(mesaj))return message.channel.send(motion)
+    if(!mesaj || isNaN(mesaj))return message.channel.send(spany)
      
    
      if(mesaj > 600) {
        message.delete()
-    const motion = new MessageEmbed()
+    const spany = new MessageEmbed()
       .setColor("#3f007f")
       .setAuthor("Gloss Bot", "https://images-ext-2.discordapp.net/external/upcVsk1w8rbo_jfrfun5j4Fi0YCd25WPM9pu4SG-NQs/https/cdn.discordapp.com/avatars/726040861602742324/ff5a9e9abdd36d5f2effcfd9876527e2.webp")
       .setDescription(`**:no2: | 600 Dan Fazla Mesaj Silemezsin**`)
       .setFooter(`🔮 Tüm Hakları Saklıdır.`)
-      return message.channel.send(motion)       
+      return message.channel.send(spany)       
     }
 
         if (mesaj < 101) {
       message.channel.bulkDelete(args[0]).then(msg => {
-         const motion = new MessageEmbed()
+         const spany = new MessageEmbed()
            .setColor("#3f007f")
            .setAuthor("Gloss Bot", "https://images-ext-2.discordapp.net/external/upcVsk1w8rbo_jfrfun5j4Fi0YCd25WPM9pu4SG-NQs/https/cdn.discordapp.com/avatars/726040861602742324/ff5a9e9abdd36d5f2effcfd9876527e2.webp")
            .setDescription(`**:yess22: |** ${msg.size} **Adet Mesaj Silindi!**`)
            .setFooter(`🔮 Tüm Hakları Saklıdır.`)
-           return message.author.send(motion)       
+           return message.author.send(spany)       
           })
         }
         if (mesaj > 100 && mesaj < 200) {
@@ -104,10 +102,6 @@ module.exports = {
           message.channel.bulkDelete(100);
           message.channel.bulkDelete(100);
         }
-// nasıl
-//knk ctrl + alt + f ile hepsini değiştir bekle
-     // okey .D burdan 10 dk kazandık glitch özelliğimi o lan (glitch kullanmadığım için ne bilem aq) ayn
-     //pc de === ctrl h aq chromedeyiz geçmişe atıyor qw.e 
-     //glitchde === ctrl + alt + f
+
    }
 }
